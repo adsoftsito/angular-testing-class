69d891d93179a59a69c776a308827ad160145ada
adsoftsito <adsoft@live.com.mx>
2020-10-09 09:55:01 -0500

    move compute-demo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ComputeDemoComponent } from './compute/compute-demo/compute-demo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComputeDemoComponent
   ],
   imports: [
     BrowserModule
